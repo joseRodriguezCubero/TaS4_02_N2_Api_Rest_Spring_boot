1159ec77f48aacc1d99c8fffe1a4e294d8511379
joseRodriguezCubero <josanrocu@gmail.com>
2024-01-19 20:24:40 +0100

    first commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/rodriguez/jose/s04/t02/n02/controllers/FruitController.java
@@ -0,0 +1,2 @@
+package cat.itacademy.barcelonactiva.rodriguez.jose.s04.t02.n02.controllers;public class FruitController {
+}
---
+++ src/main/java/cat/itacademy/barcelonactiva/rodriguez/jose/s04/t02/n02/model/repository/IFruitRepository.java
@@ -0,0 +1,2 @@
+package cat.itacademy.barcelonactiva.rodriguez.jose.s04.t02.n02.model.repository;public interface IFruitRepository {
+}
